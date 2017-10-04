9f26031e7fd304122a52ff6e778582c25b0e662b
Rodrigo Elo <rodrigoelo@gmail.com>
2017-10-04 14:55:21 -0300

    includes header y footer

---
+++ footer.php
@@ -0,0 +1,44 @@
+</div>
+      <footer>
+          <div class="container">
+            <div class="row">
+               <div class="p-4 col-md-3">
+                  <h2 class="mb-4 text-white">ASIENTA</h2>
+                  <p class="text-white">Siempre estamos para vos</p>
+               </div>
+               <div class="p-4 col-md-3">
+                  <h2 class="mb-4 text-white">Sitio</h2>
+                  <ul class="list-unstyled">
+                     <a href="#" class="text-white">Home</a>
+                     <br>
+                     <a href="#" class="text-white">Nosotros</a>
+                     <br>
+                     <a href="#" class="text-white">Nuestros Servicios</a>
+                     <br>
+                     <a href="#" class="text-white">Historias</a>
+                  </ul>
+               </div>
+               <div class="p-4 col-md-3">
+                  <h2 class="mb-4">Contacto</h2>
+                  <p>
+                     <a href="tel:+246 - 542 550 5462" class="text-white"><i class="fa d-inline mr-3 fa-phone text-info"></i>+549xxxxxxxxxx</a>
+                  </p>
+                  <p>
+                     <a href="mailto:info@pingendo.com" class="text-white"><i class="fa d-inline mr-3 fa-envelope-o text-info"></i>info@asienta.com</a>
+                  </p>
+                  <p>
+                     <a href="https://goo.gl/maps/AUq7b9W7yYJ2" class="text-white" target="blank"><i class="fa d-inline mr-3 fa-map-marker text-info"></i>Alguna direccion</a>
+                  </p>
+               </div>
+               <div class="p-4 col-md-3">
+                  <h2 class="mb-4 text-light">Suscribite</h2>
+                  <form>
+                     <fieldset class="form-group text-white"> <label for="exampleInputEmail1">A nuestro newsletter</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email"> 
+                     </fieldset>
+                     <button type="submit" class="btn btn-outline-light">Enviar</button>
+                  </form>
+               </div>
+            </div>
+          </div>
+      </footer>
---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+<?php include 'header.php'; ?>
+
+<!-- /.ACÁ ARRANCA EL FORM 
+<form class="navbar-form navbar-right">
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search">
+        </div>
+        <button type="submit" class="btn btn-default">Submit</button>
+</form> -->
+<div class="row">
+  
+  <div class="col-lg-6">
+    <div class="input-group">
+      <input type="text" class="form-control" aria-label="...">
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Categorías </button>
+        <!-- <span class="caret"></span> 
+        <ul class="dropdown-menu dropdown-menu-right">
+          <li><a href="#">Categorías</a></li>
+          <li><a href="#">Another action</a></li>
+          <li><a href="#">Something else here</a></li>
+          <li role="separator" class="divider"></li>
+          <li><a href="#">Separated link</a></li>
+        </ul> /btn-group -->
+      </div><!-- /btn-group -->
+    </div><!-- /input-group -->
+  </div><!-- /.col-lg-6 -->
+</div><!-- /.row -->
+<img class="img-slide-home" src="images/SLIDE1.jpg">
+<div class="container">
+  <div class="row">
+    <div class="col-sm-12 col-md-6">
+      <div class="thumbnail">
+        <img src="images/categoria_exteriores.jpg" alt="...">
+        
+      </div><!-- /thumbnail-->
+    </div><!-- /col-sm-6 col-md-4 -->
+     <div class="col-sm-12 col-md-6">
+      <div class="thumbnail">
+        <img src="images/categoria_exteriores.jpg" alt="...">
+        
+      </div><!-- /thumbnail-->
+    </div><!-- /col-sm-6 col-md-4 -->
+  </div><!-- /row -->
+  <div class="jumbotron" style="background-color: white; text-align: center;">
+    <h4>Productos destacados</h4>
+  </div>
+</div>  
+<div class="container">
+  <div class="row">
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="images/producto1.jpg" alt="...">
+      <div class="caption">
+        <h4>Banco madera jardín</h4>
+        <p>Producto nacoennabdjbnwkaejbfkwjbn</p>
+        <h5>$3.500</h5>
+        <p><a href="#" class="btn btn-primary" role="button">Agregar a carrito</a> <a href="#" class="btn btn-default" role="button">Comprar</a></p>
+      </div>
+    </div>
+  </div><!-- /col-sm-6 col-md-4 -->
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="images/producto1.jpg" alt="...">
+      <div class="caption">
+        <h4>Banco madera jardín</h4>
+        <p>Producto nacoennabdjbnwkaejbfkwjbn</p>
+        <h5>$3.500</h5>
+        <p><a href="#" class="btn btn-primary" role="button">Agregar a carrito</a> <a href="#" class="btn btn-default" role="button">Comprar</a></p>
+      </div>
+    </div>
+  </div><!-- /col-sm-6 col-md-4 -->
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="images/producto1.jpg" alt="...">
+      <div class="caption">
+        <h4>Banco madera jardín</h4>
+        <p>Producto nacoennabdjbnwkaejbfkwjbn</p>
+        <h5>$3.500</h5>
+        <p><a href="#" class="btn btn-primary" role="button">Agregar a carrito</a> <a href="#" class="btn btn-default" role="button">Comprar</a></p>
+      </div>
+    </div>
+  </div><!-- /col-sm-6 col-md-4 -->
+  <div class="col-sm-6 col-md-3">
+    <div class="thumbnail">
+      <img src="images/producto1.jpg" alt="...">
+      <div class="caption">
+        <h4>Banco madera jardín</h4>
+        <p>Producto nacoennabdjbnwkaejbfkwjbn</p>
+        <h5>$3.500</h5>
+        <p><a href="#" class="btn btn-primary" role="button">Agregar a carrito</a> <a href="#" class="btn btn-default" role="button">Comprar</a></p>
+      </div>
+    </div>
+  </div><!-- /col-sm-6 col-md-4 -->
+</div>
+ 
+<?php include 'footer.php' ?>
+</body>
+</html>
+
