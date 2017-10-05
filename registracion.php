d9f5adc0e6247b82e1ef06e9874b795bb6727be3
Franco Caputo <francocaputomovies@gmail.com>
2017-10-05 17:43:51 -0300

    Cambiamos inicio y register

--- registracion.php
+++ registracion.php
@@ -18,60 +18,30 @@
     </div>
   </div><!-- /.container-fluid -->
 </nav>
+   <div class="row"> 
+        <div class="col-md-offset-4 col-md-4"> 
+            <div class="form-login" style="background-color: #967760; padding: 30px; margin: 31px 72px;"> 
+            <h4 style="color: white;">Registrarse</h4> 
+            <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="nombre completo" /> 
+            </br> 
+            <input type="email" id="userEmail" class="form-control input-sm chat-input" placeholder="email" /> 
+            </br> 
+            <input type="password" id="userPassword" class="form-control input-sm chat-input" placeholder="contraseña" /> 
+            </br> 
+            <input type="password" id="userPasswordC" class="form-control input-sm chat-input" placeholder="confirmar contraseña" /> 
+            </br> 
+            <div class="wrapper"> 
+            <span class="group-btn">      
+                <a href="#" class="btn btn-default btn-md"> Ingresar <i class="fa fa-sign-in"></i></a> 
+            </span> 
+            </div> 
+            </div> 
+         
+        </div> 
+      </div> 
+    </div> <!-- /.row --> 
+</div><!-- /.container --> 
 
-
-<div class="container">
-  <div class="row">
-    <div id='col-md-4 col-sm-8 col-xs-10'>
-      <div class="registration">
-        <form id='register' action='confirmacion.php' method='POST'>
-            <fieldset >
-                <legend>Registrate</legend>
-
-                <input type='hidden' name='submitted' id='submitted' value='1'/>
-
-                <div class='short_explanation'>* campos requeridos</div>
-                
-                <div><span class='error'></span></div>
-                <div class='section-form'>
-                    <label for='name' >Nombre completo: </label><br/>
-                    <input type='text' name='name' id='name' value='' maxlength="50" /><br/>
-                    <span id='register_name_errorloc' class='error'></span>
-                </div>
-                <div class='section-form'>
-                    <label for='email' >Email:</label><br/>
-                    <input type='text' name='email' id='email' value='' maxlength="50" /><br/>
-                    <span id='register_email_errorloc' class='error'></span>
-                </div>
-                <div class='section-form'>
-                    <label for='edad' >Edad:</label><br/>
-                    <input type='text' name='edad' id='edad' value='' maxlength="2" /><br/>
-                    <span id='register_email_errorloc' class='error'></span>
-                </div>
-                <div class='section-form'>
-                    <label for='username' >Nombre de usuario*:</label><br/>
-                    <input type='text' name='username' id='username' value='' maxlength="50" /><br/>
-                    <span id='register_username_errorloc' class='error'></span>
-                </div>
-                <div class='section-form' style='height:80px;'>
-                    <label for='password' >Contaseña*:</label><br/>
-                    <div class='pwdwidgetdiv' id='thepwddiv' ></div>
-                    <input type='password' name='password' id='password' maxlength="50" />
-                    <div id='register_password_errorloc' class='error'></div>
-                </div>
-                <div class="section-form">
-                <label><input type="checkbox" name="checkbox" id="cbox1" value="first_checkbox"><p class="short_explanation"> Acepto bases y condiciones</p> </label><br>
-                </div>
-                <div class='section-form-submit'>
-                    <input type='submit' name='Submit' value='Enviar' />
-                </div>
-
-            </fieldset>
-          </form>
-        </div>
-      </div>
-    </div> <!-- /.row -->
-</div><!-- /.container -->
       <?php require_once("footer.php"); ?>
 </body>
 </html>
