a2d724b620cf819a04dfb89143acf359571331bf
Rodrigo Elo <rodrigoelo@gmail.com>
2017-11-02 14:11:44 -0300

    Auto stash before merge of "master" and "origin/master"
    FAQ

--- preguntas-frecuentes.php
+++ preguntas-frecuentes.php
@@ -11,9 +11,7 @@
         </h4>
       </div>
       <div id="collapse1" class="panel-collapse collapse in">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        <div class="panel-body">Podes realizar tu compra con tarjeta de credito como asi tambien con transferencia bancaria y pago contra entrega.</div>
       </div>
     </div>
     <div class="panel panel-default">
@@ -23,9 +21,7 @@
         </h4>
       </div>
       <div id="collapse2" class="panel-collapse collapse">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        <div class="panel-body">Despachamos tu compra a todo el pais por expreso.</div>
       </div>
     </div>
     <div class="panel panel-default">
@@ -35,33 +31,27 @@
         </h4>
       </div>
       <div id="collapse3" class="panel-collapse collapse">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+        <div class="panel-body">Realizamos entregas en Capital Federal y GBA con costo adicional segun el volumen y la distancia.</div>
       </div>
     </div>
     <div class="panel panel-default">
       <div class="panel-heading">
         <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">4- ¿A dónde llamo si todavía no me ha llegado el artículo que compré?</a>
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse4">4- ¿A dónde llamo si todavía no me ha llegado el artículo que compré?</a>
         </h4>
       </div>
-      <div id="collapse2" class="panel-collapse collapse">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+      <div id="collapse4" class="panel-collapse collapse">
+        <div class="panel-body">Podes comunicarte al 0810-45454545 las 24hs.</div>
       </div>
     </div>
     <div class="panel panel-default">
       <div class="panel-heading">
         <h4 class="panel-title">
-          <a data-toggle="collapse" data-parent="#accordion" href="#collapse2">5- ¿Dónde reclamo si el artículo entregado está en mal estado?</a>
+          <a data-toggle="collapse" data-parent="#accordion" href="#collapse5">5- ¿Puedo ver los muebles personalmente?</a>
         </h4>
       </div>
-      <div id="collapse2" class="panel-collapse collapse">
-        <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+      <div id="collapse5" class="panel-collapse collapse">
+        <div class="panel-body">Tenemos un Showrrom en Capital federal para poder ver los muebles personalmente.</div>
       </div>
     </div>
   </div> 
