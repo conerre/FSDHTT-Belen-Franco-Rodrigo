304db7daaacbb87aec5056dd0f9c376f7fa325aa
Rodrigo Elo <rodrigoelo@gmail.com>
2017-12-15 16:17:22 -0300

    admin

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -1,6 +1,20 @@
 @extends("masterPage")
 @section("principal")
 
+<div class="row admin-row-prod">
+	<div class="col-xs-12 col-md-4 text-center">
+	<a href="/agregarProducto"><button type="button" class="btn btn-info">Agregar producto</button></a><br>
+	</div>
+	<div class="col-xs-12 col-md-4 text-center">
+	<a href="/productos"><button type="button" class="btn btn-info">Editar producto</button></a><br>
+	</div>
+	<div class="col-xs-12 col-md-4 text-center">
+	<a href="/agregarCategoria"><button type="button" class="btn btn-info">Agregar categoría</button></a>
+	</div>
+	<div class="col-xs-12 col-md-4 text-center">
+	<a href="/productos"><button type="button" class="btn btn-info">Listado</button></a>
+	</div>
+</div>	
 <div class="table-responsive">
 <table class="table table-hover">
 	 <thead>
