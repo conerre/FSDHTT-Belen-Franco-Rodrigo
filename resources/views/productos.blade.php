54ff677be2357b8f049c4e3cb89ac121fd355368
Rodrigo Elo <rodrigoelo@gmail.com>
2017-12-15 15:09:18 -0300

    admin con tabla

--- resources/views/productos.blade.php
+++ resources/views/productos.blade.php
@@ -1,15 +1,43 @@
 @extends("masterPage")
 @section("principal")
 
-<ul>
-	@foreach($products as $product)
-		<li>
-			<a href="/producto/{{$product->id}}">{{$product->name}}</a>
+
+<div class="table-responsive">
+<table class="table table-hover">
+	 <thead>
+	 	<tr>
+        <th>img</th>
+        <th>Nombre</th>
+        <th>Precio</th>
+        <th>Stock</th>
+        <th>codigo</th>
+    	</tr>
+      </thead>
+
+<tbody>
+		@foreach($products as $product)
+	<tr>
+		
+			<td>
+			<a href="/producto/{{$product->id}}"><img class="fotoeditar" src="{{$product->rutaThumbnail()}}"> </a>
+			</td>
+
+			<td><a href="/producto/{{$product->id}}">{{$product->name}}</a></td>
+			<td><a href="/producto/{{$product->id}}">{{$product->price}}</a></td>
+			<td><a href="/producto/{{$product->id}}">{{$product->stock}}</a></td>
+			<td><a href="/producto/{{$product->id}}">{{$product->article_code}}</a></td>
+	
 			@if(session('usuario')->type===2)
-				<a href="/borrarProducto/{{$product->id}}"><button type="button" class="btn btn-danger">Eliminar producto</button></a>
-				<a href="/editarProducto/{{$product->id}}"><button type="button" class="btn btn-warning">Editar producto</button></a>
+				<td>
+				<a href="/borrarProducto/{{$product->id}}"><button type="button" class="btn btn-danger">Eliminar</button></a>
+				<a href="/editarProducto/{{$product->id}}"><button type="button" class="btn btn-warning">Editar</button></a>
+			</td>
+			
 			@endif
-		</li>
-	@endforeach 
-<ul>
+			
+		</tr>
+	@endforeach
+	</tbody>
+</table>
+</div>
 @endsection
